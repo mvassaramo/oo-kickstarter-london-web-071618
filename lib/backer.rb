3dea18f084d2be79e05c1953c616582e8d13092a
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-19 18:45:47 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer 
+  
+  attr_reader :project
+  attr_accessor :name, :backed_projects 
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end 
+  
+  def back_project(project)
+    @project = project 
+    @backed_projects << project
+    @backed_projects << project.add_backer(self)
+  end 
+  
+  
+end
---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project 
+  
+  attr_reader :title 
+  attr_accessor :backers  
+  
+  def initialize(title)
+  @title = title 
+  @backers = []
+  end 
+  
+  def add_backer(backer) 
+    @backer = backer 
+    @backers << backer
+    backer.backed_projects << self
+  end 
+  
+  
+end 
